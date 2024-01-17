db684f0a16034201c3cf524cce8e3260f3676261
C191239 <zsz2007314zsz@sina.com>
2024-01-17 12:09:56 +0800

    type and namespace

--- lib/types/req.ts
+++ lib/types/req.ts
@@ -3,90 +3,121 @@
 * Use `Req` prefix.
 */
 
-import { CmnDateRange, CmnPassword, CmnPhoneClass, CmnPriority, CmnTagElement } from "./common";
+import { CmnDateRange, CmnPassword, CmnPhoneClass, CmnPriority, CmnStatus, CmnTagElement } from "./common";
 
-export interface ReqRegister {
-    /**
-     * 验证码
-     */
-    captcha: number;
-    email: string;
-    /**
-     * 用户姓名
-     */
-    name: string;
-    password: string;
-    phone: CmnPhoneClass | number;
-    /**
-     * 学工号
-     */
-    school_id: string;
-    tags?: CmnTagElement[];
-}
+export namespace Req {
+    export interface Register {
+        /**
+         * 验证码
+         */
+        captcha: number;
+        email: string;
+        /**
+         * 用户姓名
+         */
+        name: string;
+        password: string;
+        phone: CmnPhoneClass | number;
+        /**
+         * 学工号
+         */
+        school_id: string;
+        tags?: CmnTagElement[];
+    }
 
-export interface ReqEmail {
-    email: string;
-}
+    export interface Email {
+        email: string;
+    }
 
-export interface ReqPasswordPair {
-    email: string;
-    password: string;
-}
+    export interface PasswordPair {
+        email: string;
+        password: string;
+    }
 
-export interface ReqBulkUserInfo {
-    ids: string[];
-}
+    export interface BulkUserInfo {
+        ids: string[];
+    }
 
-export interface ReqResetPassword {
-    /**
-     * 验证码
-     */
-    captcha: number;
-    email: string;
-    new_password: string;
-}
+    export interface ResetPassword {
+        /**
+         * 验证码
+         */
+        captcha: number;
+        email: string;
+        new_password: string;
+    }
 
-export interface ReqUserInfo {
-    departments?: string[] | null;
-    /**
-     * 姓名
-     */
-    name?: null | string;
-    password?: null | CmnPassword;
-    phone?: CmnPhoneClass | number | null;
-    /**
-     * 学工号
-     */
-    school_id?: null | string;
-    /**
-     * Token过期秒数，0为不过期
-     */
-    token_expire_duration?: number | null;
-}
+    export interface UserInfo {
+        departments?: string[] | null;
+        /**
+         * 姓名
+         */
+        name?: null | string;
+        password?: null | CmnPassword;
+        phone?: CmnPhoneClass | number | null;
+        /**
+         * 学工号
+         */
+        school_id?: null | string;
+        /**
+         * Token过期秒数，0为不过期
+         */
+        token_expire_duration?: number | null;
+    }
 
-export interface ReqSetPerms {
-    permissions: string[];
-    /**
-     * 目标用户id
-     */
-    target_account: string;
-}
+    export interface SetPerms {
+        permissions: string[];
+        /**
+         * 目标用户id
+         */
+        target_account: string;
+    }
 
-export interface ReqPost {
-    /**
-     * 是否连续
-     */
-    grouped: boolean;
-    notes: string;
-    priority: CmnPriority;
-    /**
-     * 资源id列表
-     */
-    resources: string[];
-    time: CmnDateRange;
-    title: string;
-}
+    export interface Post {
+        /**
+         * 是否连续
+         */
+        grouped: boolean;
+        notes: string;
+        priority: CmnPriority;
+        /**
+         * 资源id列表
+         */
+        resources: string[];
+        time: CmnDateRange;
+        title: string;
+    }
 
-export interface ReqBulkPosts{
-    posts: string;
+    export interface BulkPosts {
+        posts: string;
+    }
+
+    export interface Modify {
+        /**
+         * 是否连续
+         */
+        grouped?: boolean | null;
+        /**
+         * 备注，附加备注
+         */
+        notes?: null | string;
+        resources?: string[] | null;
+        time?: null | CmnDateRange;
+        title?: null | string;
+    }
+
+    export interface Review {
+        /**
+         * 附加信息
+         */
+        message?: null | string;
+        status: CmnStatus;
+    }
+
+    export type BulkRemove = {
+        type: "Posts";
+        posts: string;
+    } | {
+        type: "Unused";
+    }
 }
--- lib/types/res.ts
+++ lib/types/res.ts
@@ -5,79 +5,81 @@
 
 import { CmnDateRange, CmnPhoneClass, CmnPriority, CmnState, CmnStatus, CmnTagElement, CmnPostType } from "./common";
 
-export interface ResLogin {
+export namespace Res {
+    export interface Login {
+        /**
+         * 过期时间，timestamp
+         */
+        expire_at: number | null;
+        /**
+         * 用户id
+         */
+        id: string;
+        token: string;
+    }
+
     /**
-     * 过期时间，timestamp
+     * AccountInfo
      */
-    expire_at: number | null;
+    export interface Account {
+        departments?: string[];
+        email: string;
+        /**
+         * 用户姓名
+         */
+        name: string;
+        phone?: CmnPhoneClass | number;
+        /**
+         * 学工号
+         */
+        school_id?: string;
+        tags?: CmnTagElement[];
+        /**
+         * Token过期秒数
+         */
+        token_expire_duration?: number | null;
+        type: "Full" | "Owned" | "Simple";
+    }
+
+    export type BulkAccount = Map<string, Account>;
+
+    export interface PostId {
+        id: string;
+    }
+
+    export interface PostIds {
+        posts: string[];
+    }
+
     /**
-     * 用户id
+     * PostInfo
      */
-    id: string;
-    token: string;
+    export interface Post {
+        creator?: string;
+        grouped: boolean;
+        priority: CmnPriority;
+        resources: string[];
+        states?: CmnState[];
+        time?: CmnDateRange;
+        title: string;
+        type: CmnPostType;
+    }
+
+    /**
+     * State
+     */
+    export interface PostState {
+        message: string;
+        /**
+         * 操作人
+         */
+        operator: string;
+        status: CmnStatus;
+        /**
+         * timestamp
+         */
+        time: number;
+    }
+
+    export type BulkPosts = Map<string, Post>;
 }
-
-/**
- * AccountInfo
- */
-export interface ResAccount {
-    departments?: string[];
-    email: string;
-    /**
-     * 用户姓名
-     */
-    name: string;
-    phone?: CmnPhoneClass | number;
-    /**
-     * 学工号
-     */
-    school_id?: string;
-    tags?: CmnTagElement[];
-    /**
-     * Token过期秒数
-     */
-    token_expire_duration?: number | null;
-    type: "Full" | "Owned" | "Simple";
-}
-
-export type ResBulkAccount = Map<string, ResAccount>;
-
-export interface ResPostId {
-    id: string;
-}
-
-export interface ResPostIds {
-    posts: string[];
-}
-
-/**
- * PostInfo
- */
-export interface ResPost {
-    creator?: string;
-    grouped: boolean;
-    priority: CmnPriority;
-    resources: string[];
-    states?: CmnState[];
-    time?: CmnDateRange;
-    title: string;
-    type: CmnPostType;
-}
-
-/**
- * State
- */
-export interface ResPostState {
-    message: string;
-    /**
-     * 操作人
-     */
-    operator: string;
-    status: CmnStatus;
-    /**
-     * timestamp
-     */
-    time: number;
-}
-
-export type ResBulkPosts = Map<string, ResPost>;
